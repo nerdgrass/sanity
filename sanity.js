0b9717b040706338a97e23dc22473f8675c0ade1
nerdgrass <arboerstler@gmail.com>
2015-12-05 16:25:57 -0600

    adds user account creation & auth

--- sanity.js
+++ sanity.js
@@ -2,22 +2,31 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
+  //
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Meteor.methods({
+      // checkTwitter: function () {
+      //     this.unblock();
+      //     return Meteor.http.call("GET", "http://search.twitter.com/search.json?q=perkytweets");
+      // }
+    });
   });
 }
